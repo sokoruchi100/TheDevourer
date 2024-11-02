fe223b61d7360112d8dc9af9c07e607339ea8b30
sokoruchi100 <harmeyforever5435@gmail.com>
2024-11-02 01:45:38 -0400

    changed basename to public url

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import TableOfContents from "./components/TableOfContents";
+import { BrowserRouter } from "react-router-dom";
 
 import Chapter from "./components/Chapter";
 
 function App() {
   return (
-    <Router basename="/TheDevourer">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/toc" element={<TableOfContents />} />
         <Route path="/chapter/:chapterId" element={<Chapter />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
